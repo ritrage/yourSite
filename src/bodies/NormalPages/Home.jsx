947951aed76033d3d5b533d1eb35fd15f03351c6
Ritwik Sharma <sharmaritwik98@gmail.com>
2022-02-20 03:20:04 +0530

    third commit

--- src/bodies/NormalPages/Home.jsx
+++ src/bodies/NormalPages/Home.jsx
@@ -30,7 +30,7 @@ export default function Home() {
       <Grid container alignContent={'space-around'}>
         <Grid item md={4} lg={6}
         sx={{display:{xs:'none', sm:'none', md:'flex', lg:'flex'}}}>
-            <Img alt="yourSite" src="https://enterpriseleague.com/blog/wp-content/uploads/growth-tips.jpg" />
+            <Img alt="yourSite" src="https://disruptiveadvertising.com/static/81d94fc8d7b425af4e8247fab792dc68/e57f1/business-growth-goal-blog.jpg" />
         </Grid>
         <Grid item xs={12} sm={10} md={8} lg={6}>
           <Item>
